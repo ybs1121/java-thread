28cc26643e58d07af39106ac6ce16655a1944f13
ybs991 <ybs991@naver.com>
2024-08-05 16:04:56 +0900

    1. 동시성 문제 2. 임계영역 3. synchronized 메소드, 코드블럭

---
+++ src/thread/sync/test/SyncTest1Main.java
@@ -0,0 +1,40 @@
+package thread.sync.test;
+
+public class SyncTest1Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+         Runnable task = new Runnable() {
+
+             @Override
+             public void run() {
+                 for (int i = 0; i < 10000; i++) {
+                     counter.increment();
+                 }
+             }
+         };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println("결과 : " + counter.getCount());
+
+    }
+
+
+    static class Counter {
+        private int count = 0;
+
+        public synchronized void increment() {
+            count += 1;
+        }
+
+        public synchronized int getCount() {
+            return count;
+        }
+    }
+}
